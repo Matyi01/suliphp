0884a9ba032a20a7801e4fa2df3372827443f126
Matyi01 <holczer.matyas.21@ady-nagyatad.hu>
2025-11-05 13:13:25 +0100

    asd

--- php1/php_szallitoszalag/szalag.php
+++ php1/php_szallitoszalag/szalag.php
@@ -3,9 +3,9 @@ include_once "utils.php";
 
 function feladat2()
 {
-    $vissza = "<form action='".$_SERVER['PHP_SELF']."' method='get'>";
-    $vissza .= "<labal for='szallitasid' >Adja meg melyik adatokra kíváncsi:</label>";
-    $vissza .= "<input type='number' name='szallitasid' id='szallitasid'>";
+    $vissza = "<form action='" . $_SERVER['PHP_SELF'] . "' method='get'>";
+    $vissza .= "<label for='szallitasid'>Adja meg melyik adatokra kíváncsi:</label>";
+    $vissza .= "<input type='number' name='szallitasid' id='szallitasid' >";
     $vissza .= "<input type='submit' value='Küldés'>";
     $vissza .= "</form>";
     return $vissza;
@@ -29,6 +29,22 @@ while (!feof($myfile)) {
     }
 }
 fclose($myfile);
+
+if (isset($_GET['szallitasid'])) {
+    if ($_GET["szallitasid"] == "") {
+        $szid = -1;
+    }
+    else {
+        $szid = $_GET['szallitasid'] - 1;
+    }
+    if (isset ($adatok[$szid])) {
+    echo "<div>Honnan: <strong>" . $adatok[$szid][1] . "</strong></div>
+    <div>Hova: <strong>" . $adatok[$szid][2] . "</strong></div>";
+    }
+    else {
+        echo "<div>Nincs ".($szid+1)." azonosítójú szállítás!</div>";
+    }
+}
 
 echo feladat2();
 
