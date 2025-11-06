8c59af9c6278cfe07f290906874ae545d0dd6992
Matyi01 <holczer.matyas.21@ady-nagyatad.hu>
2025-11-06 12:23:31 +0100

    asd

--- php1/file_feltolt/index.php
+++ php1/file_feltolt/index.php
@@ -1,48 +1,54 @@
 <?php
-include("utils.php");
-$target_dir = "fajlFeltoltes/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+include "../fugvenyek.php";
+$target_dir = "../IMAGES/";
+$target_file = $target_dir . "Eredeti/" . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+//echo $_SERVER["DOCUMENT_ROOT"];
+//echo PATHINFO_EXTENSION;
 
-
-echo(PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
+if (isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-  if($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
+  if ($check !== false) {
     $uploadOk = 1;
   } else {
-     
-    echo "File is not an image.";
     $uploadOk = 0;
   }
 }
 
 if ($uploadOk == 0) {
-  echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+    $meret = getimagesize($target_file);
+    if ($meret[0] > $meret[1]) {
+      $ujX = 100;
+      $ujY = round((100 / $meret[0]) * $meret[1]);
+    } else {
+      $ujX = 100;
+      $ujY = round((100 / $meret[1]) * $meret[0]);
+    }
+    $kicsi = imagecreatetruecolor($ujX, $ujY);
+    $forras = imagecreatefrompng($target_file);
+    imagecopyresized($kicsi, $forras, 0, 0, 0, 0, $ujX, $ujY, $meret[0], $meret[1]);
   } else {
-    echo "Sorry, there was an error uploading your file.";
   }
 }
 ?>
 <!DOCTYPE html>
 <html lang="hu">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
+
 <body>
-    <form action="feltolt.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
+  <form action="feltoltTovabbFejleszt.php" method="post" enctype="multipart/form-data">
+    Select image to upload:
+    <input type="file" name="fileToUpload" id="fileToUpload">
+    <input type="submit" value="Upload Image" name="submit">
+  </form>
 </body>
+
 </html>
