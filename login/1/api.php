659cc7cde8d851ab29a4fb8dfd735870f11e710b
Matyi01 <holczer.matyas.21@ady-nagyatad.hu>
2025-12-18 12:25:25 +0100

    logiiin

--- login/1/api.php
+++ login/1/api.php
@@ -13,7 +13,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $data = json_decode(file_get_contents("php://input"), true);
 
-    echo password_hash($data["password"], PASSWORD_DEFAULT);
+    if ($apiParts[0] == "login") {
+
+        $query = "SELECT id, password FROM users WHERE username = '" .
+            mysqli_real_escape_string($conn, $data["username"]) . "'";
+
+        $results = mysqli_query($conn, $query);
+
+        $users = [];
+
+        while ($row = mysqli_fetch_assoc($results)) {
+            $users[] = $row;
+        }
+
+        if (sizeof($users) == 1 && password_verify($data["password"], $users[0]["password"])) {
+            //tkoen generálás
+            $token = bin2hex(random_bytes(32));
+            $expires = date("Y-m-d H:i:s", time() + 1800); //0,5 óra múlva lejár
+            //echo $token;
+            //var_dump($expires);
+            $query = "INSERT INTO token (userid, token, datum) VALUES (" .
+                intval($users[0]["id"]) . ", '" .
+                mysqli_real_escape_string($conn, $token) . "', '" .
+                mysqli_real_escape_string($conn, $expires) . "')";
+        } else {
+            echo "nem jó";
+        }
+
+    }
 
 }
 
